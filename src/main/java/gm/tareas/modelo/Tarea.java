04493315cbf7cba9dfddf698ef7d341b7b6e92ee
Luis Gonzalez <luisgnzhdz@gmail.com>
2025-03-17 00:23:06 -0600

    [FEAT]: add the JavaFx to the project

---
+++ src/main/java/gm/tareas/modelo/Tarea.java
@@ -0,0 +1,31 @@
+package gm.tareas.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Tarea {
+
+  // The following code create the columns in the database table
+  // @GeneratedValue(strategy = GenerationType.IDENTITY) is used to auto increment the idTarea column
+  // @Entity is used to create a table in the database 
+  // the variables are the columns in the table
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer idTarea;
+  private String nombreTarea;
+  private String responsable;
+  private String estatus;
+
+  
+}
